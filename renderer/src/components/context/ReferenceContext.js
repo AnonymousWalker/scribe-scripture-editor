cdb0f2abd2ac297dc944c9efe79e3945129972e4
Tony Anh Tran <hoanganhtran1998@gmail.com>
2025-07-22 13:53:40 -0400

    Revert "debug rtl"
    This reverts commit 345f77fbd96c8ab2322ef3720a392c3875d39be0.

--- renderer/src/components/context/ReferenceContext.js
+++ renderer/src/components/context/ReferenceContext.js
@@ -137,7 +137,7 @@ export default function ReferenceContextProvider({ children }) {
                       break;
                     case 'textTranslation':
                       setBookmarksVerses(resources.project?.textTranslation?.bookMarks);
-                      setProjectScriptureDir("rtl");//resources.project?.textTranslation?.scriptDirection?.toUpperCase());
+                      setProjectScriptureDir(resources.project?.textTranslation?.scriptDirection?.toUpperCase());
                       setSelectedFont(resources.project?.textTranslation?.font);
                       setEditorFontSize(resources.project?.textTranslation?.fontSize || 1);
                       break;
